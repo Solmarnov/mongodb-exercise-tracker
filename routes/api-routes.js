b2fecf7aed3c236abc8ee6afff343f2fc9d684e0
Solmarnov <solmarnov@gmail.com>
2020-06-14 00:18:13 +1000

    Add models and connect createWorkout

---
+++ routes/api-routes.js
@@ -0,0 +1,24 @@
+const db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  });
+
+  app.post("/api/workouts", ({ body }, res) => {
+    console.log(body);
+    db.Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+  });
+};
